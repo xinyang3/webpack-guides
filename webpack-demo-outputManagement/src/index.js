4a8b0774cd9e6f31d457750a05001d2fe27875d3
zhuxinyang <zhu.xinyang@h3c.com>
2018-09-25 23:28:06 +0800

    webpack-demo 生茶环境&&开发环境

--- webpack-demo-outputManagement/src/index.js
+++ webpack-demo-outputManagement/src/index.js
@@ -1,18 +1,45 @@
 import _ from 'lodash';
 import printMe from './print.js';
 
-  function component() {
-    var element = document.createElement('div');
-   var btn = document.createElement('button');
+// function component() {
+//   var element = document.createElement('div');
+//  var btn = document.createElement('button');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-   btn.innerHTML = 'Click me and check the console!';
-   btn.onclick = printMe;
+//  btn.innerHTML = 'Click me and check the console!';
+//  btn.onclick = printMe;
 
-   element.appendChild(btn);
+//  element.appendChild(btn);
 
-    return element;
-  }
+//   return element;
+// }
 
-  document.body.appendChild(component());
+// document.body.appendChild(component());
+
+
+import {
+  cube
+} from './math.js';
+
+function component() {
+  var element = document.createElement('pre');
+
+  // lodash 是由当前 script 脚本 import 导入进来的
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!');
+    printMe();
+  })
+}
--- webpack-demo-outputManagement/webpack.config.js
+++ webpack-demo-outputManagement/webpack.config.js
@@ -1,38 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-            app: './src/index.js',
-            print: './src/print.js'
-       },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-            new HtmlWebpackPlugin({
-                title: 'Output Management'
-            })
-    ],
-    output: {
-        filename: 'bundle.js',
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-   module: {
-     rules: [
-       {
-         test: /\.css$/,
-         use: [
-           'style-loader',
-           'css-loader'
-         ]
-       },
-       {
+  entry: {
+    print: './src/print.js',
+    app: './src/index.js',
+    math: './src/math.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: 'bundle.js',
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-        'file-loader'
+          'file-loader'
         ]
-       }
-     ]
-   }
+      }
+    ]
+  }
 };
+
